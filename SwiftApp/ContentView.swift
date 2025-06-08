b57bf4f8d037cfb8fb1221bf7f67a5791d6b4527
Bipul <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2025-06-08 11:58:34 +0600

    gdg

--- SwiftApp/ContentView.swift
+++ SwiftApp/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isTapped = false
+    @State private var isTapped2 = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+            Image("imagevai")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .cornerRadius(100)
+                            .padding()
+            
+            Image(systemName: "cloud.sun")
+                .font(.system(size: 100))
+                .foregroundColor(.blue)
+            Text("Hello World").font(.system(size: 20))
+                .foregroundColor(.cyan)
         }
         .padding()
+        
+        Image(isTapped ? "imagevai" : "bipul")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .cornerRadius(100).shadow(radius: 10)
+                .onTapGesture {
+                    self.isTapped.toggle()
+                }
+        Image( systemName: isTapped2 ? "cloud.sun" :"cloud.sun.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .onTapGesture {
+                    self.isTapped2.toggle()
+                }
     }
 }
 
