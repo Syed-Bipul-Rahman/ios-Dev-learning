b26808692a5dda91ea52131c8552f85dcd2d20dc
Bipul <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2025-06-08 10:11:50 +0600

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Bipul on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftAppTests/SwiftAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppTests.swift
+//  SwiftAppTests
+//
+//  Created by Bipul on 8/6/25.
+//
+
+import Testing
+@testable import SwiftApp
+
+struct SwiftAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
